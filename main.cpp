27b8f3bf88647e372d0fe438d8f2d2467cf95a8c
RyanVrecenar <Ryanvre@yahoo.com>
2014-02-09 15:40:15 -0800

    added database functionality to main

--- main.cpp
+++ main.cpp
@@ -5,43 +5,52 @@ using namespace std;
 
 int main()
 {
+	database database();
+	//attributes list
 	string name = "animals";
 	vector<string> attribute_names;
 	attribute_names.push_back("name");
 	attribute_names.push_back("kind");
 	attribute_names.push_back("years");
 
+	//primary keys list
 	vector<string> primary_key;
 	primary_key.push_back("name");
 	primary_key.push_back("kind");
 
-	table t (name, attribute_names, primary_key); //create table
+	//creation of table using above
+	table t (name, attribute_names, primary_key); 
 
+	//creating tuples
 	vector<string> joe;
 	joe.push_back("joe");
 	joe.push_back("dog");
 	joe.push_back("4");
 
+	//inserting first tuple
 	t.insert(joe);
 
+	//creating tuples
 	vector<string> bob;
 	bob.push_back("bob");
 	bob.push_back("cat");
 	bob.push_back("4");
 
+	//inserting second tuple
 	t.insert(bob);
 
 	t.display_table();
 
+	cout<<t.entity_table[1]==t.entity_table[2];
+	
 	cout << "\n\n";
 
 	t.update("years = 6", "years < 5");
 
 	t.display_table();
-
-	entity a();
-	entity b();
-
+	
+	database.insert(t);
+	
 	cin.get();
 	return 0;
 }
