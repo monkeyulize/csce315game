b74d7e8317848e0596ef8ab049b0402b4198f3f5
RyanVrecenar <Ryanvre@yahoo.com>
2014-02-09 16:06:49 -0800

    database.cpp

---
+++ database.cpp
@@ -0,0 +1,25 @@
+#include "database.h"
+void set_union(string view_name, string table_one_name, string table_two_name)		//: compute the union of two relations; the relations must be union-compatible.
+{
+
+}
+void set_differnce(string view_name, string table_one_name, string table_two_name)	//: compute the set difference of two relations; the relations must be union-compatible.
+{
+
+}
+void set_selection( string view_name, string table_name, int tuple_index)	//: select the tuples in a relation that satisfy a particular condition.
+{
+
+}
+void set_projection(string view_name, string table_name, vector<string> attributes)	//: select a subset of the attributes in a relation.
+{
+
+}
+void set_renaming(string view_name, string table_name, vector<string> attributes)	//: rename the attributes in a relation.
+{
+
+}
+void set_cross_product(string view_name, string table_one_name, string table_two_name)//: compute the Cartesian product of two relations.
+{
+
+}
