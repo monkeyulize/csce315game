a350ea910589da7d06fba4ebde01330a84e50241
RyanVrecenar <Ryanvre@yahoo.com>
2014-02-09 19:21:10 -0800

    pushing differnce

--- database.cpp
+++ database.cpp
@@ -37,8 +37,43 @@ table database::set_union(string view_name, string table_one_name, string table_
 }
 table database::set_differnce(string view_name, string table_one_name, string table_two_name)	//: compute the set difference of two relations; the relations must be union-compatible.
 {
-
-	return table();
+	//defining tables
+	table diff_table;
+	table bigger_table;
+	table smaller_table;
+	table t1 = tables[find_table(table_one_name)];
+	table t2 = tables[find_table(table_two_name)];
+	
+	if (t1.entity_table.size() > t2.entity_table.size()) {
+		bigger_table = t1;
+		smaller_table = t2;
+	}
+	else {
+		bigger_table = t2;
+		smaller_table = t1;
+	}
+	int check=0;
+	//finding differences between both tables using smaller one as outer limit
+	for (int i = 0; i < smaller_table.entity_table.size(); i++) {
+		for (int j = 0; j < bigger_table.entity_table.size(); j++) {
+			if (!(smaller_table.entity_table[i] == bigger_table.entity_table[j])) {
+				check++;
+			}
+		}
+		if(check==bigger_table.entity_table.size()) diff_table.push_back(smaller_table.entity_table[i])
+		check=0;
+	}
+	for (int i = 0; i < bigger_table.entity_table.size(); i++) {
+		for (int j = 0; j < smaller_table.entity_table.size(); j++) {
+			if (!(bigger_table.entity_table[i] == smaller_table.entity_table[j])) {
+				check++;
+			}
+		}
+		if(check==smaller_table.entity_table.size()) diff_table.push_back(bigger_table.entity_table[i])
+		check=0;
+	}
+	
+	return diff_table;
 }
 table database::set_selection(string view_name, string table_name, int tuple_index)	//: select the tuples in a relation that satisfy a particular condition.
 {
