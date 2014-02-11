97713540484a34de41aad5c49e800a8359e9dfee
ryanvrecenar <ryanvre@yahoo.com>
2014-02-10 21:15:52 -0600

    rename may be done checking

--- database.cpp
+++ database.cpp
@@ -76,7 +76,13 @@ table database::set_projection(string view_name, string table_name, vector<strin
 }
 table database::set_renaming(string view_name, string table_name, vector<string> attributes)	//: rename the attributes in a relation.
 {
-	return table();
+	table t1 = tables[find_table(table_name)];
+	for(int i = 0; i < t1.attribute_names.size(); i++)		//go through all attributes
+	{
+		t1.attribute_names[i] = attributes[i];
+	}
+	t1.set_name(view_name);
+	return t1;
 }
 table database::set_cross_product(string view_name, string table_one_name, string table_two_name)//: compute the Cartesian product of two relations.
 {
