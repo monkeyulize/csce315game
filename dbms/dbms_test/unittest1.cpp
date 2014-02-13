23e88ad7e425be01a6d77c25880c6839ae3901b3
brandonkgrey <brandon.k.grey@gmail.com>
2014-02-13 14:02:39 -0600

    wrote a primary parser test, couldn't get projection test to work

--- dbms/dbms_test/unittest1.cpp
+++ dbms/dbms_test/unittest1.cpp
@@ -11,7 +11,7 @@ namespace dbms_test
 	{
 	public:
 		
-		TEST_METHOD(UnionTest)
+		TEST_METHOD(Union_Test)
 		{
 			
 			database db;
@@ -59,25 +59,90 @@ namespace dbms_test
 			db.get_table("Correct Union").insert(bob);
 			db.get_table("Correct Union").insert(marty);
 			db.get_table("Correct Union").insert(spot);
-
-			//bool equal = false;
-			//if(unioned == db.get_table("Correct Union")) { equal = true; }
 			
 			Assert::IsTrue(unioned == db.get_table("Correct Union"));
-			//Assert::AreEqual(unioned, db.get_table("Correct Union"));
+			
 		}
 
-		TEST_METHOD(ParserTest)
+		TEST_METHOD(Project_Test)
 		{
+
+			database db;
+
+			string name = "animals";
+
+			vector<string> attribute_names;
+			attribute_names.push_back("name");
+			attribute_names.push_back("kind");
+			attribute_names.push_back("years");
+
+			vector<string> primary_key;
+			primary_key.push_back("name");
+			primary_key.push_back("kind");
+
+			vector<string> bob;
+			bob.push_back("bob");
+			bob.push_back("cat");
+			bob.push_back("3");
+
+			vector<string> bob1;
+			bob1.push_back("bob");
+			bob1.push_back("cat");
+			
+			vector<string> marty;
+			marty.push_back("marty");
+			marty.push_back("cat");
+			marty.push_back("5");
+
+			vector<string> marty1;
+			marty1.push_back("marty");
+			marty1.push_back("cat");
+
+			vector<string> species;
+			species.push_back("kind");
+			species.push_back("name");
+
+			db.create_table("cats", attribute_names, primary_key);
+			db.get_table("cats").insert(bob);
+			db.get_table("cats").insert(marty);
+
+			db.set_projection("proj", "cats", species);
+
+			db.create_table("Projected", species, primary_key);
+			db.get_table("Projected").insert(bob1);
+			db.get_table("Projected").insert(marty1);
+
+			Assert::IsTrue(db.get_table("Projected") == db.get_table("proj"));
+
+		}
+
+		/*TEST_METHOD(Parser_Test)
+		{
+			vector<Token> parsed;
 			vector<Token> chopped;
+
 			chopped.push_back(Token(9, "CREATE TABLE"));
 			chopped.push_back(Token(9, "animals"));
 			chopped.push_back(Token(9, "name"));
 			chopped.push_back(Token(9, "VARCHAR"));
-			//chopped
+			chopped.push_back(Token(8, 20));
+			chopped.push_back(Token(9, "kind"));
+			chopped.push_back(Token(9, "VARCHAR"));
+			chopped.push_back(Token(8, 8));
+			chopped.push_back(Token(9, "years"));
+			chopped.push_back(Token(9, "INTEGER"));
+			chopped.push_back(Token(9, "PRIMARY KEY"));
+			chopped.push_back(Token(9, "name"));
+			chopped.push_back(Token(9, "kind"));
+
+			parsed = parser("CREATE TABLE animals (name VARCHAR(20), kind VARCHAR(8), years INTEGER) PRIMARY KEY (name, kind)");
 
 
-		}
+			Assert::IsTrue(chopped == parsed);
+
+
+
+		}*/
 
 		
 
