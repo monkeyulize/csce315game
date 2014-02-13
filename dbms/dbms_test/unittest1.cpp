ef371036941d2ddb91a7e9774700e55a6b3bda2d
drm8026 <dmil14@yahoo.com>
2014-02-13 14:12:06 -0600

    made test for difference and selection selection still needs attention

--- dbms/dbms_test/unittest1.cpp
+++ dbms/dbms_test/unittest1.cpp
@@ -66,17 +66,118 @@ namespace dbms_test
 			Assert::IsTrue(unioned == db.get_table("Correct Union"));
 			//Assert::AreEqual(unioned, db.get_table("Correct Union"));
 		}
-
-		TEST_METHOD(ParserTest)
+		
+		
+		TEST_METHOD(DifferenceTest)
 		{
-			vector<Token> chopped;
-			chopped.push_back(Token(9, "CREATE TABLE"));
-			chopped.push_back(Token(9, "animals"));
-			chopped.push_back(Token(9, "name"));
-			chopped.push_back(Token(9, "VARCHAR"));
-			//chopped
+			
+			database db;
+			string name = "animals";
+			vector<string> attribute_names;
+			attribute_names.push_back("name");
+			attribute_names.push_back("kind");
+			attribute_names.push_back("years");
+
+			vector<string> primary_key;
+			primary_key.push_back("name");
+			primary_key.push_back("kind");
+
+			vector<string> joe;
+			joe.push_back("joe");
+			joe.push_back("dog");
+			joe.push_back("4");
+			
+			vector<string> bob;
+			bob.push_back("bob");
+			bob.push_back("cat");
+			bob.push_back("3");
+			
+			vector<string> marty;
+			marty.push_back("marty");
+			marty.push_back("cat");
+			marty.push_back("5");
+			
+			vector<string> spot;
+			spot.push_back("spot");
+			spot.push_back("dog");
+			spot.push_back("6");
+
+			db.create_table("dogs", attribute_names, primary_key);
+			db.create_table("cats", attribute_names, primary_key);
+			db.get_table("dogs").insert(joe);
+			db.get_table("dogs").insert(spot);
+			db.get_table("cats").insert(bob);
+			db.get_table("cats").insert(marty);
+
+			table difference = db.set_difference("pets", "cats", "dogs");
+
+			db.create_table("Correct Difference", attribute_names, primary_key);
+			db.get_table("Correct Difference").insert(joe);
+			db.get_table("Correct Difference").insert(bob);
+			db.get_table("Correct Difference").insert(marty);
+			db.get_table("Correct Difference").insert(spot);
+
+			//bool equal = false;
+			//if(unioned == db.get_table("Correct Union")) { equal = true; }
+			
+			Assert::IsTrue(difference == db.get_table("Correct Difference"));
+			//Assert::AreEqual(unioned, db.get_table("Correct Union"));
+		}
+
+		TEST_METHOD(SelectionTest)
+		{
+			
+			database db;
+			string name = "animals";
+			vector<string> attribute_names;
+			attribute_names.push_back("name");
+			attribute_names.push_back("kind");
+			attribute_names.push_back("years");
+
+			vector<string> primary_key;
+			primary_key.push_back("name");
+			primary_key.push_back("kind");
+
+			vector<string> joe;
+			joe.push_back("joe");
+			joe.push_back("dog");
+			joe.push_back("4");
+						
+			vector<string> spot;
+			spot.push_back("spot");
+			spot.push_back("dog");
+			spot.push_back("6");
+
+			db.create_table("dogs", attribute_names, primary_key);
+			db.get_table("dogs").insert(joe);
+			db.get_table("dogs").insert(spot);
 
 
+	/*	
+		felines.push_back("cat");
+		table selection = db.set_selection("sell", "cats", felines);
+		selection = db.set_selection("sell", "cats", felines);
+		selection.display_table();
+		felines.push_back("6");
+		table temp6 = db.set_selection("sell", "dogs", felines);
+		temp6.display_table();*/
+			vector<string> felines;
+			felines.push_back("dog");
+
+			table selection = db.set_selection("sell", "dog", felines);
+			/*table selection = db.set_selection("bob", "cat", bob);
+			table selection = db.set_selection("marty", "cat", marty);
+			table selection = db.set_selection("spot", "dog", spot);*/
+			
+			db.create_table("Correct Selection", attribute_names, primary_key);
+			db.get_table("Correct Selection").insert(joe);
+			db.get_table("Correct Selection").insert(spot);
+
+			//bool equal = false;
+			//if(unioned == db.get_table("Correct Union")) { equal = true; }
+			
+			Assert::IsTrue(selection == db.get_table("Correct Selection"));
+			//Assert::AreEqual(unioned, db.get_table("Correct Union"));
 		}
 
 		
