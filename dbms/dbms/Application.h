40be61910cac12c23802d765441a3b777220b507
RyanVrecenar <Ryanvre@yahoo.com>
2014-02-22 21:41:15 -0800

    Adding menu functionality

--- dbms/dbms/Application.h
+++ dbms/dbms/Application.h
@@ -5,6 +5,11 @@ class Application{
 		database db;
 		parser p;
 	public:
+		void print_tables(){
+			for (int i = 0; i < db.get_tables_vector().size(); i++){
+				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name() << endl;
+			}
+		}
 		void display_menu(){
 			cout << "-------------------------------------------------------------------------------		" << endl;
 			cout << "                                    Commands and Queries                              	" << endl;
@@ -12,7 +17,7 @@ class Application{
 			cout << "                                         Add<FILL>                      				" << endl;
 			cout << "                                       Remove<FILL>									" << endl;
 			cout << "                               Combine(add, subtract, cross)                           " << endl;
-			cout << "                                       Diplay<FILL>									" << endl;
+			cout << "                                       Display<FILL>									" << endl;
 			cout << "                                   New<FILL>Collection                  				" << endl;
 			cout << "                                        Save<FILL>										" << endl;
 			cout << "                                        Open<FILL>               						" << endl;
@@ -25,102 +30,80 @@ class Application{
 			cout << "-------------------------------------------------------------------------------		" << endl;
 			cout << "                                    Commands and Queries                              	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Add<FILL> : Add a <Fill> to a specific collection.             				" << endl;
-			cout << "        Remove<FILL> : Remove a specific or collection of <Fills> that 				" << endl;
-			cout << "						meet a condition." 												  << endl;
-			cout << "        Combine : Perform an operation on collections of <Fills>					" << endl;
+			cout << "        Add<FILL>     : Add a <Fill> to a specific collection.             				" << endl;
+			cout << "        Remove<FILL>  : Remove a specific or collection of <Fills> that 				" << endl;
+			cout << "		                meet a condition." 												  << endl;
+			cout << "        Combine       : Perform an operation on collections of <Fills>					" << endl;
 			cout<<  "                       Add, Cross, Difference."							              << endl;
 			cout << "        Display<FILL> : Display the contents of a collection with specified 			" << endl;
-			cout << "		     	 		 name.		"                                                     << endl;
+			cout << "		                 name.		"                                                     << endl;
 			cout << "        New<FILL>Collection : Creates a new collection of <Fills> 						" << endl;
-			cout << "        Save<FILL>	  :	Saves values to a file for future reading						" << endl;
-			cout << "        Open<FILL>   : Opens and reads from a file a <Fill>       						" << endl;
+			cout << "        Save<FILL>    :	Saves values to a file for future reading						" << endl;
+			cout << "        Open<FILL>    : Opens and reads from a file a <Fill>       						" << endl;
 			cout << "        SaveAndCLose<FILL> : Closes and saves values to a file 						" << endl;
-			cout << "        Help :	You should know you are here, but it prints						" << endl;
+			cout << "        Help          :	You should know you are here, but it prints						" << endl;
 			cout << "    					detailed description of each instruction						" << endl;
-			cout << "        Exit(To terminate)	:															" << endl;
+			cout << "        Exit(To terminate)	: Terminates Application    								" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
 		}
 		void prompt_add(){
-			string table1, table2;
+			string table1;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to perform a combine on?                          	" << endl;
+			cout << "        Which <Fill> would you like to add to?                          	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the first tables name----------------------------------------------------------" << endl;
 			cin >> table1;
-			cout << "----------Enter the second tables name----------------------------------------------------------" << endl;
-			cin >> table2;
 		}
 		void prompt_combine(){
 			string table1, table2;
 			cout << "-------------------------------------------------------------------------------		" << endl;
 			cout << "        Which <Fill> would you like to perform a combine on?                          	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the first tables name----------------------------------------------------------" << endl;
 			cin >> table1;
 			cout << "----------Enter the second tables name----------------------------------------------------------" << endl;
 			cin >> table2;
 		}
 		void prompt_remove(){
-			string table1, table2;
+			string table1;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to perform a combine on?                          	" << endl;
+			cout << "        Which <Fill> would you like to remove from?                          	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the first tables name----------------------------------------------------------" << endl;
 			cin >> table1;
-			cout << "----------Enter the second tables name----------------------------------------------------------" << endl;
-			cin >> table2;
 		}
 		void prompt_display(){
-			string table1, table2;
+			string table1;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to perform a combine on?                          	" << endl;
+			cout << "        Which <Fill> do you want to display				                        	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the first tables name----------------------------------------------------------" << endl;
 			cin >> table1;
-			cout << "----------Enter the second tables name----------------------------------------------------------" << endl;
-			cin >> table2;
 		}
 		void prompt_new_table(){
 			string table1, table2;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to perform a combine on?                          	" << endl;
+			cout << "        What is the name of the new <Fill> Collection		                         	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
-			cout << "----------Enter the first tables name----------------------------------------------------------" << endl;
 			cin >> table1;
-			cout << "----------Enter the second tables name----------------------------------------------------------" << endl;
-			cin >> table2;
 		}
 		void prompt_save(){
 			string table1;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to save?                          					" << endl;
+			cout << "				 Which <Fill> would you like to save?                  					" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the Tables name -------------------------------------------------------" << endl;
 			cin >> table1;
 		}
 		void prompt_open(){
 			string table1;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			cout << "        Which <Fill> would you like to open?                          	" << endl;
+			cout << "        What is the name of the <Fill> you would like to open?                       	" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
 			cin >> table1;
 		}
@@ -129,9 +112,7 @@ class Application{
 			cout << "-------------------------------------------------------------------------------		" << endl;
 			cout << "        Which <Fill> would you like to save and close?                          					" << endl;
 			cout << "-------------------------------------------------------------------------------		" << endl;
-			for (int i = 0; i < db.get_tables_vector().size(); i++){
-				cout << "Table " << i << ": " << db.get_tables_vector()[i].get_name();
-			}
+			print_tables();
 			cout << "----------Enter the Tables name -------------------------------------------------------" << endl;
 			cin >> table1;
 		}
@@ -155,7 +136,7 @@ class Application{
 				else if (command == "Combine"){
 					prompt_combine();
 				}
-				else if (command == "Diplay<FILL>"){
+				else if (command == "Display<FILL>"){
 					prompt_display();
 				}
 				else if (command == "New<FILL>Collection"){
