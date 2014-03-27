4459d19fda2f62b2445d549ae49fcd17f75df2cf
brandonkgrey <brandon.k.grey@gmail.com>
2014-03-27 14:05:55 -0500

    server.js changed to receive message

--- js/server.js
+++ js/server.js
@@ -1,16 +1,30 @@
-var http = require("http");
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs')
 
-var currentdate = new Date(); 
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+app.listen(10111);
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(currentdate.toLocalTimeString());
-  response.end();
-}).listen(11111);
-console.log('Server running on host 11111');
+function handler(request, response) {
+  fs.readFile(
+	__dirname + '/index.html',
+    function(err, content) {
+      if (err) {
+        response.writeHead(500);
+        return response.end('Error loading index.html!');
+      }
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(content);
+    });
+}
+
+
+io.sockets.on( 
+	'connection', 
+	function(client) {
+		client.emit('welcome', 'Welcome to the game server');
+		client.on(
+		'update',
+		function(message) {
+		console.log(message);
+		});
+	});
