369431683b0f541435b290af21a2ac6c7ff79e38
brandonkgrey <brandon.k.grey@gmail.com>
2014-03-27 13:40:31 -0500

    server.js very basic

--- js/demo.js
+++ js/demo.js
@@ -1,21 +1,26 @@
 function init() {
 	canvas = document.createElement("canvas");
-	canvas.width = 500;
-	canvas.height = 250;
+	canvas {
+		padding-left: 0;
+		padding-right: 0;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+		width: 800px;
+	}
 	document.body.appendChild(canvas);
 	var stage = new createjs.Stage(canvas);
 	var circle = new createjs.Shape();
-	circle.graphics.beginFill("blue").drawCircle(0, 0, 50);
+	circle.graphics.beginFill("blue").drawCircle(0, 50, 50);
 	circle.x = 100;
 	circle.y = 100;
 	stage.addChild(circle);
+	
+	stage.on("stagemousemove", function(evt) {
+		circle.x = evt.stageX;
+		circle.y = evt.stageY;
+		stage.update();
+	})
 	stage.update();
 
 }
-
-Ticker.setFPS(30);
-Ticker.addListener(tick);
-function tick(e) {
-	stage.update();
-}
-
